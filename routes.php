db09deba8a6e4c245ab1c98279c9fd854cf51f92
Ken Ballon <kenneth.ballon@keymedia.com>
2024-04-17 15:18:57 +0800

    Refactor code to remove unnecessary files and controllers

--- routes.php
+++ routes.php
@@ -1,20 +1,20 @@
 <?php
 
-$router->get('/', 'controllers/index.php');
-$router->get('/about', 'controllers/about.php');
-$router->get('/contact', 'controllers/contact.php');
+$router->get('/', 'index.php');
+$router->get('/about', 'about.php');
+$router->get('/contact', 'contact.php');
 
-$router->get('/notes', 'controllers/notes/index.php')->only('auth');
-$router->get('/note', 'controllers/notes/show.php')->only('auth');
-$router->post('/notes', 'controllers/notes/store.php')->only('auth');
-$router->get('/notes/create', 'controllers/notes/create.php')->only('auth');
-$router->get('/note/edit', 'controllers/notes/edit.php')->only('auth');
-$router->patch('/note', 'controllers/notes/update.php')->only('auth');
-$router->delete('/note', 'controllers/notes/destroy.php')->only('auth');
+$router->get('/notes', 'notes/index.php')->only('auth');
+$router->get('/note', 'notes/show.php')->only('auth');
+$router->post('/notes', 'notes/store.php')->only('auth');
+$router->get('/notes/create', 'notes/create.php')->only('auth');
+$router->get('/note/edit', 'notes/edit.php')->only('auth');
+$router->patch('/note', 'notes/update.php')->only('auth');
+$router->delete('/note', 'notes/destroy.php')->only('auth');
 
-$router->get('/register', 'controllers/registration/create.php')->only('guest');
-$router->post('/register', 'controllers/registration/store.php');
+$router->get('/register', 'registration/create.php')->only('guest');
+$router->post('/register', 'registration/store.php');
 
-$router->get('/login', 'controllers/sessions/create.php')->only('guest');
-$router->post('/sessions', 'controllers/sessions/store.php')->only('guest');
-$router->delete('/sessions', 'controllers/sessions/destroy.php')->only('auth');
+$router->get('/login', 'sessions/create.php')->only('guest');
+$router->post('/sessions', 'sessions/store.php')->only('guest');
+$router->delete('/sessions', 'sessions/destroy.php')->only('auth');
